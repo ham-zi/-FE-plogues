2f67ef38deb3d47bd06c04b70b4363dd91550091
DevAJH89 <DevAJH89@gmail.com>
2026-06-29 17:10:21 +0900

    댓글컴퍼넌트

--- iso-react/src/main.jsx
+++ iso-react/src/main.jsx
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-=======
+
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
->>>>>>> dbef1a4b84db92f0929469ab261d99c29280c73e
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
