8ab279b2a969a6b7503445055f8455123689fd3b
grape-fruit-apricot <4.fear.1ess.4ra@gmail.com>
2026-07-10 13:46:24 +0900

    fix:catch 내용 수정

--- iso-react/src/features/Boards/Join/RequestModal.jsx
+++ iso-react/src/features/Boards/Join/RequestModal.jsx
@@ -28,15 +28,19 @@ const RequestModal = ({ joinNo, category }) => {
   const onSubmit = async () => {
     try {
       await api.post(`/request/${joinNo}`, request);
+      customAlert.success("참여요청 성공");
       if (category === "PLOG") {
-        customAlert.success("참여요청 성공");
         navi("/joins/plogging");
       } else {
-        customAlert.success("참여요청 성공");
         navi("/joins/plant");
       }
     } catch (err) {
-      navi("/");
+      customAlert.error("이미 참여요청한 게시글입니다.");
+      if (category === "PLOG") {
+        navi("/joins/plogging");
+      } else {
+        navi("/joins/plant");
+      }
     }
   };
 
