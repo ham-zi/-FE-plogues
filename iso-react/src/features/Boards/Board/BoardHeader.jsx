da6bcfa2a254bf19ff5dba9ce9ab0b17f26b9700
jiho0828 <njiho0828@gmail.com>
2026-07-10 14:59:25 +0900

    버튼수정

--- iso-react/src/features/Boards/Board/BoardHeader.jsx
+++ iso-react/src/features/Boards/Board/BoardHeader.jsx
@@ -1,13 +1,19 @@
+import { useAuth } from "../../../context/AuthContext";
 import { BoardTop } from "./BoardStyle";
 import { useNavigate } from "react-router-dom";
 
 function BoardHeader() {
   const navigate = useNavigate();
+  const { isLogin } = useAuth();
 
   return (
     <BoardTop>
       <h2>후기게시판</h2>
-      <button onClick={() => navigate("/reviews/write")}>✏️ 작성하기</button>
+      {isLogin && (
+        <button onClick={() => navigate("/reviews/write")}>
+          ✏️ 작성하기
+        </button>
+      )}
     </BoardTop>
   );
 }
